5b0f7f6016c007c91618ea1d662a3883591f98b6
shh <shh5946@126.com>
2018-06-11 14:46:35 +0800

    Initial Commit

---
+++ ColorsHelper/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ColorsHelper
+//
+//  Created by Si on 2018/6/11.
+//  Copyright © 2018年 Si. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
